238d30be093cf9aa5588f4ed0e5dbc63714cbf91
Tolik <Tolik@JABA-ANATOLY>
2023-04-06 00:06:28 +0300

    Добавьте файлы проекта.

---
+++ DogHttp/Controllers/DogsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DogHttp.Controllers
+{
+	public class DogsController : Controller
+	{
+		public IActionResult Index(int code)
+		{
+			string baseString = $"https://http.dog/{code}.jpg";
+			return View((object)baseString);
+		}
+	}
+}
